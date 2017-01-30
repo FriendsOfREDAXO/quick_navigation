8d756ed59a9a6341b939d9503d0a156388d52b87
Thomas Skerbis <skerbis@klxm.de>
2017-01-30 17:04:05 +0100

    Quick-Search in der Liste hinzugefügt

---
+++ fragments/quick_drop.php
@@ -0,0 +1,75 @@
+<?php
+    $toolbar = isset($this->toolbar) && $this->toolbar ? true : false;
+    $group = isset($this->group) && $this->group ? true : false;
+?>
+
+<?php if (!$toolbar && !$group): ?>
+<div class="dropdown<?= (isset($this->block) ? ' btn-block' : '')?><?= ((isset($this->class) && $this->class != '') ? ' ' . $this->class : '') ?>">
+<?php endif; ?>
+
+    <?php if ($toolbar): ?>
+    <a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown"<?= ((isset($this->disabled) && $this->disabled) ? ' disabled' : '') ?>>
+    <?php else: ?>
+    <button class="btn btn-default<?= (isset($this->block) ? ' btn-block' : '')?> dropdown-toggle" type="button" data-toggle="dropdown"<?= ((isset($this->disabled) && $this->disabled) ? ' disabled' : '') ?>>
+    <?php endif; ?>
+        <?php if (isset($this->button_prefix) && $this->button_prefix != ''): ?>
+        <?= $this->button_prefix ?>
+        <?php endif; ?>
+        <?php if (isset($this->button_label) && $this->button_label != ''): ?>
+        <?= ' <b>' . $this->button_label . '</b>' ?>
+        <?php endif; ?>
+        <span class="caret"></span>
+    <?php if ($toolbar): ?>
+    </a>
+    <?php else: ?>
+    </button>
+    <?php endif; ?>
+    
+	 <ul class="list-group dropdown-menu<?= (isset($this->right) ? ' dropdown-menu-right' : '')?><?= (isset($this->block) ? ' btn-block' : '')?>" role="menu">
+    	
+    	
+        <?php if (isset($this->header) && $this->header != ''): ?>
+            <li class="dropdown-header"><?= $this->header ?></li>
+           
+        <?php endif; ?> <input id="search" type="text" class="form-control input-lg" placeholder="suche" />
+        <?php
+        foreach ($this->items as $item) {
+            echo '<li' . ((isset($item['active']) && $item['active']) ? ' class="active"' : '') . (isset($item['attributes']) ? ' ' . trim($item['attributes']) : '') . '>';
+            echo(isset($item['href']) && $item['href'] != '') ? '<a href="' . $item['href'] . '">' . $item['title'] . '</a>' : $item['title'];
+            echo ' </li>';
+        }
+        ?>
+        <?php if (isset($this->footer) && $this->footer != ''): ?>
+            <li class="divider"></li>
+            <li><?= $this->footer ?></li>
+        <?php endif; ?>
+    </ul>
+
+<script>
+	$(function(){
+	$('#search').keyup(function(){	
+		var current_query = $('#search').val();
+		if (current_query !== "") {
+			$(".list-group li").hide();
+			$(".list-group li").each(function(){
+				var current_keyword = $(this).text();
+				
+			    var upercase = current_query.substr(0,1).toUpperCase() + current_query.substr(1);
+			    
+			
+				if ((current_keyword.indexOf(current_query) >=0) ||  (current_keyword.indexOf(upercase) >=0)) {
+				$(this).show();    	 	
+				};
+				
+			});    	
+		} else {
+			$(".list-group li").show();
+		};
+	});
+});
+	
+</script>
+
+<?php if (!$toolbar && !$group): ?>
+</div>
+<?php endif;

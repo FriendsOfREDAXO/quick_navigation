b8ab03e39a0de428676a85b307eb1e2324899d16
Thomas Skerbis <skerbis@klxm.de>
2017-02-10 19:50:22 +0100

    Busting wieder eingefügt

--- boot.php
+++ boot.php
@@ -21,6 +21,6 @@ if (rex::isBackend() && is_object(rex::getUser())) {
 
 if (rex::isBackend() && rex::getUser() && rex::getUser()->hasPerm('quick_navigation[]')) {
     rex_extension::register('PAGE_TITLE', 'QuickNavigation::get');
-    rex_view::addCssFile($this->getAssetsUrl('quicknavi.css'));
-    rex_view::addJsFile($this->getAssetsUrl('quicknavi.js'));
+    rex_view::addCssFile($this->getAssetsUrl('quicknavi.css?v=' . $this->getVersion()));
+    rex_view::addJsFile($this->getAssetsUrl('quicknavi.js?v=' . $this->getVersion()));
 }

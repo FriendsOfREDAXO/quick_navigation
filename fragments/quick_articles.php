ec8852838961c48cf627843e12bfc2f97e753ba5
Thomas Skerbis <skerbis@klxm.de>
2017-02-18 20:41:48 +0100

    his file is part of the Quick Navigation package.

--- fragments/quick_articles.php
+++ fragments/quick_articles.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * This file is part of the Quick Navigation package.
+ *
+ * @author (c) Friends Of REDAXO
+ * @author <friendsof@redaxo.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 $drophistory = $date = $link = $where = '';
 if (rex::getUser()->hasPerm('quick_navigation[history]')) {
             

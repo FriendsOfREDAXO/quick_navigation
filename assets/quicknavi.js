b4a20f4f1163f3d618e7ca87cdb04300882c97ad
Thomas Skerbis <skerbis@klxm.de>
2017-03-19 16:30:40 +0100

    js gekürzt

--- assets/quicknavi.js
+++ assets/quicknavi.js
@@ -47,11 +47,3 @@ $(document).on('shown.bs.dropdown', function(event) {
         dropdown.find('.dropdown-menu li:first-child #qsearch').focus();
     }, 10);
 });
-
-$(document).on('hidden.bs.dropdown', function(event) {
-    var dropdown = $(event.target);
-     
-    dropdown.find('.dropdown-menu').attr('aria-expanded', false);
-
-    dropdown.find('.dropdown-toggle').focus();
-});

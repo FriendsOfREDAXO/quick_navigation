bff944aaabb0402798489d496efd036c8351b6bb
Thomas Skerbis <thomas.skerbis@mac.com>
2017-10-12 23:30:55 +0200

    Cache-Busting entfernt

--- boot.php
+++ boot.php
@@ -21,8 +21,8 @@ if (rex::isBackend() && is_object(rex::getUser())) {
 if (rex::isBackend() && rex::getUser() && rex::getUser()->hasPerm('quick_navigation[]')) {
     rex_extension::register('PAGE_TITLE', 'QuickNavigation::get');
     rex_extension::register('PAGE_TITLE', 'QuickNavigation::getmedia');
-    rex_view::addCssFile($this->getAssetsUrl('quicknavi.css?v=' . $this->getVersion()));
-    rex_view::addJsFile($this->getAssetsUrl('quicknavi.js?v=' . $this->getVersion()));
+    rex_view::addCssFile($this->getAssetsUrl('quicknavi.css'));
+    rex_view::addJsFile($this->getAssetsUrl('quicknavi.js'));
 }
 
 if (rex::isBackend() && rex::getUser() && !$this->hasConfig()) {

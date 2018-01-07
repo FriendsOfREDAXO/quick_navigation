be78827272eafeb70bdf38d2b874d6c530396275
Thomas Skerbis <thomas.skerbis@mac.com>
2018-01-07 11:30:09 +0100

    Case sensitive entfernt
    https://github.com/FriendsOfREDAXO/quick_navigation/issues/53
    Danke @eaCe

--- assets/quicknavi.js
+++ assets/quicknavi.js
@@ -1,34 +1,35 @@
 $(document).on('rex:ready', function() {
-       quicknavi_filter_init();
+	quicknavi_filter_init();
 
-       var ctype = getUrlVars()["ctype"];
-       if (ctype)
-       {  
-	   $(".quicknavi a").attr('href', function(i, h) {
-	     return h + (h.indexOf('?') != -1 ? "&ctype="+ctype : "?ctype="+ctype);
-	   		});
-       }
+	var ctype = getUrlVars()["ctype"];
+	if (ctype) {
+		$(".quicknavi a").attr('href', function(i, h) {
+			return h + (h.indexOf('?') != -1 ? "&ctype=" + ctype : "?ctype=" + ctype);
+		});
+	}
 
 });
 
 $(document).on("shown.bs.dropdown", function() {
-   $(this).find(".dropdown-menu li.bg-primary a").focus();
-   $(this).find('.dropdown-menu.quicknavi li:first-child input').focus();
+	$(this).find(".dropdown-menu li.bg-primary a").focus();
+	$(this).find('.dropdown-menu.quicknavi li:first-child input').focus();
 });
 
 function quicknavi_filter_init() {
-	$('#qsearch').keyup(function(){	
+	$('#qsearch').keyup(function() {
 		var current_query = $('#qsearch').val();
 		if (current_query !== "") {
 			$(".quicknavi.list-group li.quickitem").hide();
-			$(".quicknavi.list-group li.quickitem").each(function(){
+			$(".quicknavi.list-group li.quickitem").each(function() {
 				var current_keyword = $(this).text();
-				 var upercase = current_query.substr(0,1).toUpperCase() + current_query.substr(1);
-			    if ((current_keyword.indexOf(current_query) >=0) ||  (current_keyword.indexOf(upercase) >=0)) {
-				$(this).show();    	 	
+				var current_keyword_uppercase = current_keyword.toUpperCase();
+				var uppercase = current_query.toUpperCase();
+
+				if ((current_keyword.indexOf(current_query) >= 0) || (current_keyword_uppercase.indexOf(uppercase) >= 0)) {
+					$(this).show();
 				}
-				
-			});    	
+
+			});
 		} else {
 			$(".quicknavi.list-group li.quickitem").show();
 		}
@@ -36,9 +37,9 @@ function quicknavi_filter_init() {
 }
 
 function getUrlVars() {
-var vars = {};
-var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-vars[key] = value;
-});
-return vars;
+	var vars = {};
+	var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
+		vars[key] = value;
+	});
+	return vars;
 }

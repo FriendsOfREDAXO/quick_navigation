a58da775e6c1be44de41248d8c7a6d2c821a93ec
Thomas Skerbis <thomas.skerbis@mac.com>
2018-06-02 22:30:57 +0200

    cs fix

--- fragments/quick_drop.php
+++ fragments/quick_drop.php
@@ -11,31 +11,31 @@
     $toolbar = isset($this->toolbar) && $this->toolbar ? true : false;
     $group = isset($this->group) && $this->group ? true : false;
 ?>
-<?php if (!$toolbar && !$group): ?>
+<?php if (!$toolbar && !$group) : ?>
 <div class="dropdown<?= (isset($this->block) ? ' btn-block' : '')?><?= ((isset($this->class) && $this->class != '') ? ' ' . $this->class : '') ?>">
 <?php endif; ?>
-    <?php if ($toolbar): ?>
+    <?php if ($toolbar) : ?>
     <a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown"<?= ((isset($this->disabled) && $this->disabled) ? ' disabled' : '') ?>>
-    <?php else: ?>
+    <?php else : ?>
     <button class="btn btn-default<?= (isset($this->block) ? ' btn-block' : '')?> dropdown-toggle" accesskey="m" type="button" data-toggle="dropdown"<?= ((isset($this->disabled) && $this->disabled) ? ' disabled' : '') ?>>
     <?php endif; ?>
-        <?php if (isset($this->button_prefix) && $this->button_prefix != ''): ?>
+        <?php if (isset($this->button_prefix) && $this->button_prefix != '') : ?>
         <?= $this->button_prefix ?>
         <?php endif; ?>
-        <?php if (isset($this->button_label) && $this->button_label != ''): ?>
+        <?php if (isset($this->button_label) && $this->button_label != '') : ?>
         <?= ' <b>' . $this->button_label . '</b>' ?>
         <?php endif; ?>
         <span class="caret"></span>
-    <?php if ($toolbar): ?>
+    <?php if ($toolbar) : ?>
     </a>
-    <?php else: ?>
+    <?php else : ?>
     </button>
     <?php endif; ?>
-	 <ul class="quicknavi list-group dropdown-menu<?= (isset($this->right) ? ' dropdown-menu-right' : '')?><?= (isset($this->block) ? ' btn-block' : '')?>" role="menu">   	
-        <?php if (isset($this->header) && $this->header != ''): ?>
+     <ul class="quicknavi list-group dropdown-menu<?= (isset($this->right) ? ' dropdown-menu-right' : '')?><?= (isset($this->block) ? ' btn-block' : '')?>" role="menu">    
+        <?php if (isset($this->header) && $this->header != '') : ?>
             <li class="dropdown-header"><?= $this->header ?></li>
         <li>
-        	<ul>  
+            <ul>  
         <?php endif; ?> 
         <?php
         foreach ($this->items as $item) {
@@ -44,13 +44,13 @@
             echo ' </li>';
         }
         ?>
-        <?php if (isset($this->footer) && $this->footer != ''): ?>
+        <?php if (isset($this->footer) && $this->footer != '') : ?>
             <li class="divider"></li>
             <li><?= $this->footer ?></li>
         <?php endif; ?>
-   		</ul> 
-   	</li>
+        </ul> 
+    </li>
    </ul>
-<?php if (!$toolbar && !$group): ?>
+<?php if (!$toolbar && !$group) : ?>
 </div>
 <?php endif;

15e62894271df7e1de0d42d2a7a06e01c56b494a
Thomas Skerbis <thomas.skerbis@mac.com>
2018-07-02 00:26:41 +0200

    source reorganized

--- lib/QuickNavigation.php
+++ lib/QuickNavigation.php
@@ -36,18 +36,14 @@ class QuickNavigation
         if (rex_be_controller::getCurrentPageObject()->isPopup()) {
             return $ep->getSubject();
         }
-        // get article history from fragment
-        $drophistory = new rex_fragment();
-        $drophistory->setVar('limit', '15');
-        $drophistory = $drophistory->parse('quick_articles.php');
-        
+     
         // get requested language
         $qlang= rex_request('clang', 'int');
         if ($qlang==0) {
             $qlang = 1;
         }
         
-        // get data from sked AddOn from fragment
+        // AddOn specific :: get data from sked AddOn from fragment
         $dropsked = '';
         $sked_datas = rex_addon::get('quick_navigation')->getConfig('quicknavi_sked'.$qn_user);
         if ($sked_datas != '1') {
@@ -57,12 +53,17 @@ class QuickNavigation
             }
         }
        
-        // get data from yForm AddOn from fragment
+        // AddOn specific :: get data from yForm AddOn from fragment
         $dropyform = '';
         if (rex_addon::get('yform')->isAvailable()) {
             $dropyform = new rex_fragment();
-            $dropyform->setVar('clang', $qlang);
             $dropyform = $dropyform->parse('quick_yform.php');
+        }
+        
+        // AddOn specific :: set watson button if addon is available and button is active
+        $watson = '';
+        if (rex_addon::get('watson')->isAvailable() and rex_config::get('watson', 'toggleButton', 0)==1) {
+            $watson = '<div class="btn-group"><button class="btn btn-default watson-btn">Watson</button></div>';
         }
 
         // get favorites from fragment
@@ -74,18 +75,18 @@ class QuickNavigation
         // get complete quick navi cats from fragment 
         $droplist = '';
         $droplist = new rex_fragment();
-        $droplist->setVar('clang', $qlang);
         $droplist = $droplist->parse('quick_cats.php');
-
-        // set watson button if addon is available and button is active
-        $watson = '';
-        if (rex_addon::get('watson')->isAvailable() and rex_config::get('watson', 'toggleButton', 0)==1) {
-            $watson = '<div class="btn-group"><button class="btn btn-default watson-btn">Watson</button></div>';
-        }
+        
+        // get article history from fragment
+        $drophistory = new rex_fragment();
+        $drophistory->setVar('limit', '15');
+        $drophistory = $drophistory->parse('quick_articles.php');
 
         // generate output ep for custom buttons after default set.
         $custom_buttons ='';
         $custom_buttons = rex_extension::registerPoint(new rex_extension_point('QUICK_NAVI_CUSTOM', $custom));
+        
+        // Output
         return '<div class="btn-group quicknavi-btn-group pull-right">' . $watson . $droplist . $drophistory . $dropyform . $dropsked . $dropfavs . $custom_buttons . '</div>' . $ep->getSubject();
     }
 }

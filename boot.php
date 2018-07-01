619ab7ccd34e114eba99dc18630ca9ae21840322
Thomas Skerbis <thomas.skerbis@mac.com>
2018-07-02 00:50:54 +0200

    linkmap support

--- boot.php
+++ boot.php
@@ -20,7 +20,8 @@ if (rex::isBackend() && rex::getUser()) {
 
 if (rex::isBackend() && rex::getUser() && rex::getUser()->hasPerm('quick_navigation[]')) {
     rex_extension::register('PAGE_TITLE', 'QuickNavigation::get');
-    rex_extension::register('MEDIA_LIST_TOOLBAR', 'QuickNavigation::getmedia');
+    rex_extension::register('PAGE_TITLE', 'QuickNavigation::linkmap_list');
+    rex_extension::register('MEDIA_LIST_TOOLBAR', 'QuickNavigation::media_history');
     rex_view::addCssFile($this->getAssetsUrl('quicknavi.css'));
     rex_view::addJsFile($this->getAssetsUrl('quicknavi.js'));
 }

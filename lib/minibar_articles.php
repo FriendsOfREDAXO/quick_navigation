fe6eab164b5e863fdb5c99a1858f9997a1bf8e36
Thomas Skerbis <thomas.skerbis@mac.com>
2019-05-21 14:52:41 +0200

    Update minibar_articles.php

--- lib/minibar_articles.php
+++ lib/minibar_articles.php
@@ -1,6 +1,12 @@
 <?php
 /**
- * @package redaxo\core\minibar
+ * This file is part of the Quick Navigation package.
+ *
+ * @author (c) Friends Of REDAXO
+ * @author <friendsof@redaxo.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 class rex_minibar_element_quicknavi extends rex_minibar_element
 {

d632fb264c3ac8ca91b0fccd4bcc6b2361dc6a07
Thomas Skerbis <thomas.skerbis@mac.com>
2021-01-30 21:40:22 +0100

    Update quick_button.php

--- fragments/quick_button.php
+++ fragments/quick_button.php
@@ -1,6 +1,6 @@
 <div class="btn-group">
      <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">
-     <i class="<?= (isset($this->icon) ? 'fa fa-star-o' : 'fa fa-bullseye')?>" aria-hidden="true"></i>
+     <i class="<?= (isset($this->icon) ? $this->icon : 'fa fa-bullseye')?>" aria-hidden="true"></i>
      <span class="caret"></span>
      </button>
      <ul class="quickfiles quicknavi dropdown-menu dropdown-menu-right">

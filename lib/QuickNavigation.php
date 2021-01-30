09f2a619f686012aaa5e5cc2c60138264d6ec1e6
Thomas Skerbis <thomas.skerbis@mac.com>
2021-01-30 22:18:47 +0100

    media navi now in class
    ausgäbe nun per quick button

--- lib/QuickNavigation.php
+++ lib/QuickNavigation.php
@@ -4,7 +4,7 @@
  * This file is part of the Quick Navigation package.
  *
  * @author (c) Friends Of REDAXO
- * @author     <friendsof@redaxo.org>
+ * @author  <friendsof@redaxo.org>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -17,9 +17,7 @@ class QuickNavigation
         // get media history from fragment
         if (rex_be_controller::getCurrentPagePart(1) == 'mediapool') {
             $subject = $ep->getSubject();
-            $drophistory = new rex_fragment();
-            $drophistory->setVar('limit', '15');
-            $drophistory = $drophistory->parse('quick_media.php');
+            $drophistory = self::get_media();
             $custom_media_buttons = $custom_media = '';
             $custom_media_buttons = rex_extension::registerPoint(new rex_extension_point('QUICK_NAVI_CUSTOM_MEDIA', $custom_media));
             $button = $custom_media_buttons . '<div class="input-group-btn quickmedia clearfix">' . $drophistory . '</div><select name="rex_file_category"';
@@ -39,7 +37,7 @@ class QuickNavigation
             if ($qlang == 0 || $qlang == '') {
                 $qlang = 1;
             }
-            $droplist = QuickNavigation::get_favs('linkmap');
+            $droplist = self::get_favs('linkmap');
 
             // get article history from fragment
             $drophistory = new rex_fragment();
@@ -187,7 +185,79 @@ class QuickNavigation
             return $fragment->parse('quick_button.php');
         }
     }
+ 
+    public static function get_media($limit=15)
+    {
+        $filename = $entryname = $date = $link = $where = '';
+        $opener ='';
+        $opener = rex_request('opener_input_field');
+        if (rex::getUser()->hasPerm('quick_navigation[history]')) {
+            $file_id = rex_request('file_id', 'int');
+            $quick_file_nav = '';
+            if ($file_id) {
+                $quick_file = rex_sql::factory();
+                $quick_file->setQuery('select * from ' . rex::getTablePrefix() . 'media where id=?', [$file_id]);
+    
+                $quick_file_before = rex_sql::factory();
+                $quick_file_before->setQuery('SELECT * FROM ' . rex::getTablePrefix() . 'media WHERE category_id = '. $quick_file->getValue('category_id') .' AND updatedate > ? ORDER BY updatedate LIMIT 1', [$quick_file->getValue('updatedate')]);
 
+                $quick_file_after = rex_sql::factory();
+                $quick_file_after->setQuery('SELECT * FROM ' . rex::getTablePrefix() . 'media WHERE category_id = '. $quick_file->getValue('category_id') .' AND updatedate < ? ORDER BY updatedate DESC LIMIT 1', [$quick_file->getValue('updatedate')]);
+
+                if ($quick_file_before->getRows() == 1 && $quick_file_after->getRows() == 1) {
+                    $quick_file_nav = '<a class="btn btn-default rex-form-aligned" href="'.rex_url::currentBackendPage(array_merge(['opener_input_field'=> $opener, 'file_id' => $quick_file_before->getValue('id'), 'rex_file_category' => $quick_file->getValue('category_id')])).'"><span class="fa fa-chevron-left"></span></a> - <a class="btn btn-default rex-form-aligned" href="'.rex_url::currentBackendPage(array_merge(['opener_input_field'=> $opener, 'file_id' => $quick_file_after->getValue('id'), 'rex_file_category' => $quick_file->getValue('category_id')])).'"><span class="fa fa-chevron-right"></span></a>';
+                } elseif ($quick_file_before->getRows() == 1 && !$quick_file_after->getRows() == 1) {
+                    $quick_file_nav = '<a class="btn btn-default rex-form-aligned" href="'.rex_url::currentBackendPage(array_merge(['opener_input_field'=> $opener, 'file_id' => $quick_file_before->getValue('id'), 'rex_file_category' => $quick_file->getValue('category_id')])).'"><span class="fa fa-chevron-left"></span></a>';
+                } elseif (!$quick_file_before->getRows() == 1 && $quick_file_after->getRows() == 1) {
+                    $quick_file_nav = '<a class="btn btn-default rex-form-aligned" href="'.rex_url::currentBackendPage(array_merge(['opener_input_field'=> $opener, 'file_id' => $quick_file_after->getValue('id'), 'rex_file_category' => $quick_file->getValue('category_id')])).'"><span class="fa fa-chevron-right"></span></a>';
+                }
+            }
+
+    
+            $were ='';
+            if (!rex::getUser()->hasPerm('quick_navigation[all_changes]')) {
+                $where = 'WHERE updateuser="'.rex::getUser()->getValue('login').'"';
+            }
+            $opener ='';
+            $opener = rex_request('opener_input_field');
+
+            $qry = 'SELECT category_id, id, title, filename, updateuser, updatedate FROM ' . rex::getTable('media') . ' '.$where.' ORDER BY updatedate DESC LIMIT ' . $limit;
+            $datas = rex_sql::factory()->getArray($qry);
+            $media = [];
+            if (!count($datas)) {
+                $media[] = '<li class="malert">'.rex_i18n::msg('quick_navigation_no_entries').'</li>';
+            }
+    
+            if (count($datas)) {
+                foreach ($datas as $data) {
+                    $entryname = '';
+
+                    $date = rex_formatter::strftime(strtotime($data['updatedate']), 'datetime');
+                    $href = rex_url::backendPage(
+                        'mediapool/media',
+                        [
+                'opener_input_field'=> $opener,
+                'rex_file_category' => $data['category_id'],
+                'file_id' => $data['id']
+                ]
+                    );
+
+                    if ($data['title']!='') {
+                        $entryname =   rex_escape($data['title']);
+                    } else {
+                        $entryname = rex_escape($data['filename']);
+                    }
+                    $filename = rex_escape($data['filename']);
+
+                    $media[] = '<li><a href="' . $href . '" title="' . $filename . '">' . $entryname. '<small> <i class="fa fa-user" aria-hidden="true"></i> ' . rex_escape($data['updateuser']) . ' - ' . $date . '</small></a></li>';
+                }
+            }
+            $fragment = new rex_fragment();
+            $fragment->setVar('items', $media, false);
+            $fragment->setVar('icon', 'fa fa-clock-o');
+            return $fragment->parse('quick_button.php');
+        }
+    }
 
     public static function get()
     {
@@ -223,8 +293,8 @@ class QuickNavigation
         }
 
 
-        $dropfavs = QuickNavigation::get_favs('structure');
-        $droplist = QuickNavigation::get_cats('structure');
+        $dropfavs = self::get_favs('structure');
+        $droplist = self::get_cats('structure');
 
         // get article history from fragment
         $drophistory = new rex_fragment();
@@ -240,4 +310,3 @@ class QuickNavigation
         return '<div class="btn-group quicknavi-btn-group transparent pull-right">' . $watson . $droplist . $drophistory . $dropyform . $dropsked . $dropfavs . $custom_buttons . '</div>';
     }
 }
-

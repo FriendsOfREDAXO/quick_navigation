e7fd6c2469aa4ac9efc97bb533718377a085d3f7
Thomas Skerbis <thomas.skerbis@mac.com>
2021-01-30 23:43:52 +0100

    4.0

--- lib/QuickNavigation.php
+++ lib/QuickNavigation.php
@@ -38,12 +38,7 @@ class QuickNavigation
                 $qlang = 1;
             }
             $droplist = self::get_favs('linkmap');
-
-            // get article history from fragment
-            $drophistory = new rex_fragment();
-            $drophistory->setVar('limit', '15');
-            $drophistory->setVar('mode', 'linkmap');
-            $drophistory = $drophistory->parse('quick_articles.php');
+            $drophistory = self::get_article_history('linkmap');
 
             // generate output ep for custom buttons after default set.
             $custom_linkmap_buttons = $custom = '';
@@ -145,6 +140,189 @@ class QuickNavigation
         return '<div class="btn-group">' . $fragment->parse('quick_drop.php') . '</div>';
     }
 
+    public static function get_article_history($mode='structure', $limit=15)
+    {
+        $drophistory = $date = $name = $link = $minibar = $where = $domaintitle = $status_css = $article_directions = '';
+
+
+        if ($mode == 'minibar') {
+            $icon_prefix = 'rex-minibar-icon--fa rex-minibar-icon--';
+        } else {
+            $icon_prefix = 'fa ';
+        }
+        if (rex::getUser()->hasPerm('quick_navigation[history]')) {
+            $were = '';
+            if (!rex::getUser()->hasPerm('quick_navigation[all_changes]')) {
+                $where = 'WHERE updateuser="' . rex::getUser()->getValue('login') . '"';
+            }
+
+            $qry = 'SELECT id, status, parent_id, clang_id, startarticle, name, updateuser, updatedate
+                    FROM ' . rex::getTable('article') . ' 
+                    ' . $where . ' 
+                    ORDER BY updatedate DESC
+                    LIMIT ' . $limit;
+            $datas = rex_sql::factory()->getArray($qry);
+
+            if (!count($datas)) {
+                $link .= '<li class="alert">' . rex_i18n::msg('quick_navigation_no_entries') . '</li>';
+            }
+
+            if (count($datas)) {
+                $links = [];
+                foreach ($datas as $data) {
+                    $dataID = rex_escape($data['id']);
+                    $lang = rex_clang::get($data['clang_id']);
+                    $langcode = $lang->getCode();
+                    if ($langcode) {
+                        $langcode = '<i class="fa fa-flag-o" aria-hidden="true"></i> ' . $langcode . ' - ';
+                    }
+                    $name = rex_escape($data['name']);
+                    $date = rex_formatter::strftime(strtotime($data['updatedate']), 'datetime');
+
+                    if ($mode == 'linkmap') {
+                        $href = "javascript:insertLink('redaxo://" . $dataID . "','" . $name . " [" . $dataID . "]');";
+                    } else {
+                        $href = rex_url::backendPage(
+                            'content/edit',
+                            [
+                        'mode' => 'edit',
+                        'clang' => $data['clang_id'],
+                        'category_id' => $data['parent_id'],
+                        'article_id' => $data['id']
+                    ]
+                        );
+                    }
+
+                    if (rex_addon::get('yrewrite')->isAvailable()) {
+                        if (count(rex_yrewrite::getDomains()) > 2) {
+                            $domain = rex_yrewrite::getDomainByArticleId($data['id']);
+                            if ($domain) {
+                                $domaintitle = '<br><i class="fa fa-globe" aria-hidden="true"></i> ' . rex_escape($domain);
+                            }
+                        }
+                    }
+                    $status_css = ' qn_status_' . $data['status'];
+                    $link .= '<li class=""><a class="quicknavi_left ' . $status_css . '" href="' . $href . '" title="' . $name . '">' . $name . '<small>' . $langcode . '<i class="' . $icon_prefix . 'fa-user" aria-hidden="true"></i> ' . rex_escape($data['updateuser']) . ' - ' . $date . $domaintitle . '</small></a>';
+                    $link .= '<span class="quicknavi_right"><a class ="' . $status_css . '" href="' . rex_getUrl($dataID) . '" title="' .  $name . ' ' . rex_i18n::msg('quick_navigation_title_eye') . '" target="blank"><i class="' . $icon_prefix . 'fa-eye" aria-hidden="true"></i></a></span></li>';
+                    $links[] = $link;
+                    $minibar .= $link;
+                    $link = '';
+                }
+            }
+
+            if ($mode != 'minibar') {
+                if (rex_be_controller::getCurrentPage() == 'content/edit') {
+                    $predecessor = '';
+                    $successor = '';
+                    $article_stack[] = array();
+                    // Objekt der aktuellen Kategorie laden
+                    $cat = rex_category::getCurrent();
+                    if ($cat) {
+                        $article = $cat->getArticles(false);
+                    } else {
+                        $article  =  rex_article::getRootArticles();
+                    }
+                    $current_id = rex_request('article_id');
+                    if ($article  && $current_id) {
+                        if (is_array($article)) {
+                            // Artikelreihenfolge in eine Array schreiben
+                            foreach ($article as $var) {
+                                $article_stack[] = $var->getId();
+                            }
+                            $i = 0;
+                            // Zahl der Artikel ermitteln
+                            $catcount = count($article_stack);
+                            foreach ($article_stack as $var) {
+                                if ($var == $current_id) {
+                                    $successor = '
+                        <button class="btn btn-default" disabled>
+                           <span class="fa fa-chevron-right"> 
+                        </button>
+                    ';
+                                    if ($i + 1 < $catcount) {
+                                        // ID des nachfolgenden Artikels ermitteln
+                                        $next_id = $article_stack[$i + 1];
+                                        // Artikel-Objekt holen, um den Namen des vorhergehenden Artikels zu ermitteln,
+                                        // danach Link schreiben
+                                        $article = rex_article::get($next_id);
+
+                                        $href_next = rex_url::backendPage(
+                                            'content/edit',
+                                            [
+                                        'mode' => 'edit',
+                                        'clang' => rex_clang::getCurrentId(),
+                                        'category_id' => rex_request('category_id'),
+                                        'article_id' => $next_id
+                                    ]
+                                        );
+                                        $successor = '
+
+                    <a class="btn btn-default" title="' . $article->getName() . '" href="' . $href_next . '">
+                      <span class="fa fa-chevron-right"> 
+                    </a>
+                ';
+                                    }
+
+                                    // und das Ganze nochmal für den vorhergehenden Artikel
+                                    if ($i - 1 > -1) {
+                                        $prev_id = $article_stack[$i - 1];
+
+                                        $href_prev = rex_url::backendPage(
+                                            'content/edit',
+                                            [
+                                        'mode' => 'edit',
+                                        'clang' => rex_clang::getCurrentId(),
+                                        'category_id' => rex_request('category_id'),
+                                        'article_id' => $prev_id
+                                    ]
+                                        );
+
+                                        if ($i < $catcount) {
+                                            $article = rex_article::get($prev_id);
+
+
+                                            $predecessor = '
+
+                        <button class="btn btn-default" disabled>
+                           <span class="fa fa-chevron-left"> 
+                        </button>
+                    ';
+
+                                            if ($article) {
+                                                $predecessor = '
+
+                        <a class="btn btn-default" title="' . $article->getName() . '" href="' . $href_prev . '">
+                           <span class="fa fa-chevron-left"> 
+                        </a>
+                    ';
+                                            }
+                                        }
+                                    }
+                                }
+                                $i++;
+                            }
+                        }
+                        $article_directions = '
+
+    ' . $predecessor . '
+
+    ' . $successor . '
+';
+                    }
+                }
+
+                $fragment = new rex_fragment();
+                $fragment->setVar('prepend', $article_directions, false);
+                $fragment->setVar('items', $links, false);
+                $fragment->setVar('icon', 'fa fa-clock-o');
+                return $fragment->parse('quick_button.php');
+            } else {
+                return '<ul class="minibar-quickfiles">
+            '. $link.'  
+        </ul>';
+            }
+        }
+    }
     public static function get_favs($mode = 'structure')
     {
         $user =  rex::getUser()->getId();
@@ -253,7 +431,7 @@ class QuickNavigation
                 }
             }
             $fragment = new rex_fragment();
-            $fragment->setVar('prepend',$quick_file_nav, false);
+            $fragment->setVar('prepend', $quick_file_nav, false);
             $fragment->setVar('items', $media, false);
             $fragment->setVar('icon', 'fa fa-clock-o');
             return $fragment->parse('quick_button.php');
@@ -339,12 +517,8 @@ class QuickNavigation
         $dropfavs = self::get_favs('structure');
         // get categories
         $droplist = self::get_cats('structure');
-
-        // get article history from fragment
-        $drophistory = new rex_fragment();
-        $drophistory->setVar('limit', '15');
-        $drophistory->setVar('mode', 'structure');
-        $drophistory = $drophistory->parse('quick_articles.php');
+        // get article history
+        $drophistory = self::get_article_history();
 
         // generate output ep for custom buttons after default set.
         $custom = '';

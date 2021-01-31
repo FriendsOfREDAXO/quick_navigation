c709e693350fbbbcb02ff0a16bfddcb8dcf2abbb
Thomas Skerbis <thomas.skerbis@mac.com>
2021-01-31 01:14:28 +0100

    $this->link added

--- fragments/quick_button.php
+++ fragments/quick_button.php
@@ -5,8 +5,11 @@
      <span class="caret"></span>
      </button>
      <ul class="quickfiles quicknavi dropdown-menu dropdown-menu-right">
+     <?= (isset($this->link) ? $this->link : '')?>
      <?php foreach ($this->items as $item) {
      echo $item;
      }?>
      </ul>
 </div>
+
+

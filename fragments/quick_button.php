a11f017c44b3915456caa782e5395c6c506f4226
Thomas Skerbis <thomas.skerbis@mac.com>
2021-01-31 02:36:25 +0100

    Update quick_button.php

--- fragments/quick_button.php
+++ fragments/quick_button.php
@@ -4,7 +4,7 @@
      <i class="<?= (isset($this->icon) ? $this->icon : 'fa fa-bullseye')?>" aria-hidden="true"></i>
      <span class="caret"></span>
      </button>
-     <ul class="quickfiles quicknavi dropdown-menu dropdown-menu-right">
+     <ul class="quicknavi-items quicknavi dropdown-menu dropdown-menu-right">
      <?= (isset($this->link) ? $this->link : '')?>
      <?php foreach ($this->items as $item) {
      echo $item;

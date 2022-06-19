43689bb6f55d8f24fab6a931a070081a3356a744
Thomas Skerbis <thomas.skerbis@mac.com>
2022-06-19 22:29:51 +0200

    remove $this by $package

--- pages/index.php
+++ pages/index.php
@@ -1,3 +1,4 @@
 <?php
-echo rex_view::title($this->i18n('quicknavi_title')); 
+$package = rex_addon::get('quick_navigation');
+echo rex_view::title($package->i18n('quicknavi_title')); 
 rex_be_controller::includeCurrentPageSubPath();

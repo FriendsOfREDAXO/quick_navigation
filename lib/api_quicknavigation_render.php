47c9381a74e880d2572e8be20a8b5d6963685f57
Thomas Skerbis <thomas.skerbis@mac.com>
2022-12-20 22:59:43 +0100

    comments added

--- lib/api_quicknavigation_render.php
+++ lib/api_quicknavigation_render.php
@@ -1,13 +1,26 @@
 <?php
 
+/**
+ * API function for rendering the quick navigation menu.
+ */
 class rex_api_quicknavigation_render extends rex_api_function
 {
+    /**
+     * Executes the API function and sends the quick navigation HTML as the response.
+     *
+     * @return void
+     */
     public function execute()
     {
         rex_response::sendContent(QuickNavigation::get(), 'text/html');
         exit();
     }
 
+    /**
+     * Indicates that this API function does not require CSRF protection.
+     *
+     * @return bool
+     */
     public function requiresCsrfProtection()
     {
         return false;

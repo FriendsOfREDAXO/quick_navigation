98396301808b0bc3b42507319b5004140c6cce56
Thomas Skerbis <thomas.skerbis@mac.com>
2024-03-23 11:09:26 +0100

    reconstruction

--- lib/qn_registry.php
+++ lib/qn_registry.php
@@ -1,32 +1,34 @@
-<?php 
+<?pho
 namespace FriendsOfRedaxo\QuickNavigation;
 
 class ButtonRegistry
 {
     protected static $buttons = [];
 
-    public static function registerButton($buttonInstance, $priority = 10)
+    public static function registerButton(string $className, array $params = [], $priority = 10)
     {
-        self::$buttons[] = ['button' => $buttonInstance, 'priority' => $priority];
+        self::$buttons[] = ['class' => $className, 'params' => $params, 'priority' => $priority];
     }
 
     public static function getButtonsOutput(): string
     {
-        // Überprüfen, ob Buttons registriert wurden
         if (empty(self::$buttons)) {
             return ''; 
         }
-        
+
         // Sort by prio
         usort(self::$buttons, function ($a, $b) {
             return $a['priority'] <=> $b['priority'];
         });
-
+        
         $resultString = '';
         foreach (self::$buttons as $item) {
-            $button = $item['button'];
-            if (method_exists($button, 'getButton')) {
-                $resultString .= $button->getButton();
+            $class = "FriendsOfRedaxo\\QuickNavigation\\Buttons\\" . $item['class'];
+            if (class_exists($class)) {
+                $button = new $class(...$item['params']);
+                if ($button instanceof ButtonInterface && method_exists($button, 'getButton')) {
+                    $resultString .= $button->getButton();
+                }
             }
         }
         return $resultString;

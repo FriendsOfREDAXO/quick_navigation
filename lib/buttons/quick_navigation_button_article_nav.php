e320f70b58b1d31e35d41e5e42e6f519f6f08242
Thomas Skerbis <thomas.skerbis@mac.com>
2024-04-01 19:37:23 +0200

    7.0.0
    Article navigation remade

--- lib/buttons/quick_navigation_button_article_nav.php
+++ lib/buttons/quick_navigation_button_article_nav.php
@@ -9,112 +9,58 @@ use rex_clang;
 use rex_url;
 
 use function count;
-use function is_array;
 
 class ArticleNavButton implements ButtonInterface
 {
     public function get(): string
     {
+        // Initialisiere die Navigationspfeile nicht
         $article_directions = '';
         if (rex_be_controller::getCurrentPage() == 'content/edit') {
-            $predecessor = '';
-            $successor = '';
-            $article_stack = [];
-            // Objekt der aktuellen Kategorie laden
             $cat = rex_category::getCurrent();
-            if ($cat) {
-                $article = $cat->getArticles(false);
-            } else {
-                $article = rex_article::getRootArticles();
-            }
+            $articles = $cat ? $cat->getArticles(false) : rex_article::getRootArticles(false);
 
-            $current_id = rex_request('article_id');
-            if ($article && $current_id) {
-                if (is_array($article)) {
-                    // Artikelreihenfolge in eine Array schreiben
-                    foreach ($article as $var) {
-                        $article_stack[] = $var->getId();
+            // Zeige die Navigationspfeile nur an, wenn mehr als der Startartikel vorhanden ist
+            if (count($articles) > 1) {
+                // Initialisiere Buttons als deaktiviert
+                $predecessor = '<button class="btn btn-default" disabled><span class="fa fa-chevron-left"></span></button>';
+                $successor = '<button class="btn btn-default" disabled><span class="fa fa-chevron-right"></span></button>';
+
+                $article_stack = [];
+                $current_id = rex_request('article_id');
+                foreach ($articles as $article) {
+                    $article_stack[] = $article->getId();
+                }
+
+                $index = array_search($current_id, $article_stack);
+                if ($index !== false) {
+                    // Vorherigen Artikel aktivieren, wenn möglich
+                    if ($index - 1 >= 0) {
+                        $prev_id = $article_stack[$index - 1];
+                        $href_prev = rex_url::backendPage('content/edit', [
+                            'mode' => 'edit',
+                            'clang' => rex_clang::getCurrentId(),
+                            'category_id' => rex_request('category_id'),
+                            'article_id' => $prev_id,
+                        ]);
+                        $predecessor = '<a class="btn btn-default" title="' . htmlspecialchars(rex_article::get($prev_id)->getName()) . '" href="' . $href_prev . '"><span class="fa fa-chevron-left"></span></a>';
                     }
 
-                    $i = 0;
-                    // Zahl der Artikel ermitteln
-                    $catcount = count($article_stack);
-                    foreach ($article_stack as $var) {
-                        if ($var == $current_id) {
-                            $successor = '
-        <button class="btn btn-default" disabled>
-           <span class="fa fa-chevron-right">
-        </button>
-    ';
-                            if ($i + 1 < $catcount) {
-                                // ID des nachfolgenden Artikels ermitteln
-                                $next_id = $article_stack[$i + 1];
-                                // Artikel-Objekt holen, um den Namen des vorhergehenden Artikels zu ermitteln,
-                                // danach Link schreiben
-                                $article = rex_article::get($next_id);
-
-                                $href_next = rex_url::backendPage(
-                                    'content/edit',
-                                    [
-                                        'mode' => 'edit',
-                                        'clang' => rex_clang::getCurrentId(),
-                                        'category_id' => rex_request('category_id'),
-                                        'article_id' => $next_id,
-                                    ]
-                                );
-                                $successor = '
-
-    <a class="btn btn-default" title="' . $article->getName() . '" href="' . $href_next . '">
-      <span class="fa fa-chevron-right">
-    </a>
-';
-                            }
-
-                            if ($i - 1 > -1) {
-                                $prev_id = $article_stack[$i - 1];
-
-                                $href_prev = rex_url::backendPage(
-                                    'content/edit',
-                                    [
-                                        'mode' => 'edit',
-                                        'clang' => rex_clang::getCurrentId(),
-                                        'category_id' => rex_request('category_id'),
-                                        'article_id' => $prev_id,
-                                    ]
-                                );
-
-                                if ($i < $catcount) {
-                                    $article = rex_article::get($prev_id);
-
-                                    $predecessor = '
-
-        <button class="btn btn-default" disabled>
-           <span class="fa fa-chevron-left">
-        </button>
-    ';
-
-                                    if ($article) {
-                                        $predecessor = '
-
-        <a class="btn btn-default" title="' . $article->getName() . '" href="' . $href_prev . '">
-           <span class="fa fa-chevron-left">
-        </a>
-    ';
-                                    }
-                                }
-                            }
-                        }
-
-                        ++$i;
+                    // Nächsten Artikel aktivieren, wenn möglich
+                    if ($index + 1 < count($article_stack)) {
+                        $next_id = $article_stack[$index + 1];
+                        $href_next = rex_url::backendPage('content/edit', [
+                            'mode' => 'edit',
+                            'clang' => rex_clang::getCurrentId(),
+                            'category_id' => rex_request('category_id'),
+                            'article_id' => $next_id,
+                        ]);
+                        $successor = '<a class="btn btn-default" title="' . htmlspecialchars(rex_article::get($next_id)->getName()) . '" href="' . $href_next . '"><span class="fa fa-chevron-right"></span></a>';
                     }
                 }
 
-                $article_directions = '
-
-' . $predecessor . '
-
-' . $successor . '
-';
+                // Zusammenbau der Navigationspfeile, falls zutreffend
+                $article_directions = $predecessor . ' ' . $successor;
             }
         }
 

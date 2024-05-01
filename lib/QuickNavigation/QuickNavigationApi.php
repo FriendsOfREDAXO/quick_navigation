b1fdd51fcf1460f86fae7b9b0da437da14980659
Thomas Skerbis <thomas.skerbis@mac.com>
2024-05-01 22:15:32 +0200

    Update QuickNavigationApi.php

--- lib/QuickNavigation/QuickNavigationApi.php
+++ lib/QuickNavigation/QuickNavigationApi.php
@@ -1,5 +1,7 @@
 <?php
-use namespace FriendsOfRedaxo\QuickNavigation\ApiFunction\MenuRender;
+namespace FriendsOfRedaxo\QuickNavigation;
+
+use FriendsOfRedaxo\QuickNavigation\ApiFunction\MenuRender;
 
 /**
  * @deprecated since version 8.0, will be removed in version 9.0. Use <FriendsOfRedaxo\QuickNavigation\ApiFunction\MenuRender> instead.

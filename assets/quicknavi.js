de39db51fc2d9777ae0a89588e1c67391577cf01
tbaddade <thomas@redaxo.org>
2024-05-27 22:07:36 +0200

    Js Suche angepasst

--- assets/quicknavi.js
+++ assets/quicknavi.js
@@ -30,8 +30,8 @@ function quicknavi_filter_init() {
     $('#qsearch input').keyup(function() {
         var current_query = $('#qsearch input').val();
         if (current_query !== "") {
-            $(".quicknavi.list-group li.quickitem").hide();
-            $(".quicknavi.list-group li.quickitem").each(function() {
+            $('.quick-navigation-menu-body li').hide();
+            $('.quick-navigation-menu-body li').each(function() {
                 var current_keyword = $(this).text();
                 var current_keyword_uppercase = current_keyword.toUpperCase();
                 var uppercase = current_query.toUpperCase();
@@ -42,20 +42,20 @@ function quicknavi_filter_init() {
 
             });
         } else {
-            $(".quicknavi.list-group li.quickitem").show();
+            $('.quick-navigation-menu-body li').show();
         }
 
         $('.form-clear-button input[type="text"]').on('input propertychange', function() {
-        var $this = $(this);
-        var visible = Boolean($this.val());
-        $this.siblings('.form-control-clear').toggleClass('hidden', !visible);
-    }).trigger('propertychange');
+            var $this = $(this);
+            var visible = Boolean($this.val());
+            $this.siblings('.form-control-clear').toggleClass('hidden', !visible);
+        }).trigger('propertychange');
 
-    $('.form-control-clear, .clear-button').click(function() {
-        event.stopPropagation();
-        $(this).siblings('input[type="text"]').val('').trigger("keyup")
-            .trigger('propertychange').focus();
-    });
+        $('.form-control-clear, .clear-button').click(function() {
+            event.stopPropagation();
+            $(this).siblings('input[type="text"]').val('').trigger("keyup")
+                .trigger('propertychange').focus();
+        });
 
     });
 }

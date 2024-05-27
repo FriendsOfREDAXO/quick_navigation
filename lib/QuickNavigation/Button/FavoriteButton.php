0d5bc2525bbc92ec4c6d25096731b418b765f60f
tbaddade <thomas@redaxo.org>
2024-05-27 15:22:31 +0200

    Favoriten Button angepasst

--- lib/QuickNavigation/Button/FavoriteButton.php
+++ lib/QuickNavigation/Button/FavoriteButton.php
@@ -5,8 +5,10 @@ namespace FriendsOfRedaxo\QuickNavigation\Button;
 use rex;
 use rex_addon;
 use rex_category;
+use rex_clang;
 use rex_fragment;
 use rex_i18n;
+use rex_string;
 use rex_url;
 
 use function count;
@@ -16,49 +18,73 @@ class FavoriteButton implements ButtonInterface
 {
     public function get(): string
     {
-        $mode = 'structure';
-        $userId = rex::getUser()->getId();
-        $datas = rex_addon::get('quick_navigation')->getConfig('quicknavi_favs' . $userId);
-        if ($datas && count($datas) >= 1) {
-            $items = [];
-            $clang = rex_request('clang', 'int');
-            foreach ($datas as $data) {
-                if (rex_category::get($data)) {
-                    $cat = rex_category::get($data);
-                    $catName = rex_escape($cat->getName());
-                    $catId = rex_escape($cat->getId());
-                    $href = rex_url::backendPage(
-                        'content/edit',
-                        [
-                            'page' => $mode,
-                            'clang' => $clang,
-                            'category_id' => $data,
-                        ]
-                    );
-                    $addHref = rex_url::backendPage(
-                        'structure',
-                        [
-                            'category_id' => $catId,
-                            'clang' => $clang,
-                            'function' => 'add_art',
-                        ]
-                    );
-                    $items[] = '<li class="quicknavi_left"><a href="' . $href . '" title="' . $catName . '">' . $catName . '</a></li>';
-                    if ($mode === 'structure') {
-                        $items[] = '<li class="quicknavi_right"><a href="' . $addHref . '" title="' . rex_i18n::msg('quicknavi_title_favs') . ' ' .  $catName . '"><i class="fa fa-plus" aria-hidden="true"></i></a></li>';
-                    }
-                }
-            }
+        $user = rex::getUser();
 
-            $fragment = new rex_fragment();
-            if ($items !== []) {
-                $fragment->setVar('items', $items, false);
-            }
-
-            $fragment->setVar('icon', 'fa-regular fa-star');
-            return $fragment->parse('quick_button.php');
+        if (!$user) {
+            return '';
         }
 
-        return '';
+        $categoryIds = rex_addon::get('quick_navigation')->getConfig('quicknavi_favs' . $user->getId());
+
+        $listItems = [];
+        if ($categoryIds && count($categoryIds) > 0) {
+            $clangId = rex_request('clang', 'int', rex_clang::getStartId());
+            foreach ($categoryIds as $categoryId) {
+
+                if (!$user->getComplexPerm('structure')->hasCategoryPerm($categoryId)) {
+                    continue;
+                }
+
+                $category = rex_category::get($categoryId);
+                if ($category) {
+                    $name = $category->getName();
+                } elseif (0 === $categoryId)  {
+                    $name = rex_i18n::msg('root_level');
+                } else {
+                    continue;
+                }
+
+                $attributesArticleAdd = [
+                    'href' => rex_url::backendPage('structure', ['clang' => $clangId, 'category_id' => $categoryId, 'function' => 'add_art']),
+                    'title' => rex_i18n::msg('quick_navigation_favorite_article_add') . ' ' . $name,
+                ];
+
+                $attributesCategoryAdd = [
+                    'href' => rex_url::backendPage('structure', ['clang' => $clangId, 'category_id' => $categoryId, 'function' => 'add_cat']),
+                    'title' => rex_i18n::msg('quick_navigation_favorite_category_add') . ' ' . $name,
+                ];
+
+                $attributesArticleEdit = [
+                    'href' => rex_url::backendPage('content/edit', ['clang' => $clangId, 'category_id' => $categoryId]),
+                    'title' => $name,
+                ];
+
+                $listItem = '
+                        <div class="quick-navigation-item-row">
+                            <a' . rex_string::buildAttributes($attributesArticleEdit) . '>
+                                ' . rex_escape($name) . '
+                            </a>
+                            <a' . rex_string::buildAttributes($attributesCategoryAdd) . '>
+                                <i class="fa fa-folder-plus" aria-hidden="true"></i>
+                            </a>
+                            <a' . rex_string::buildAttributes($attributesArticleAdd) . '>
+                                <i class="fa fa-file-medical" aria-hidden="true"></i>
+                            </a>
+                        </div>
+                    ';
+
+                $listItems[] = $listItem;
+            }
+        } else {
+            $fragment = new rex_fragment();
+            $listItems[] = $fragment->parse('QuickNavigation/NoResult.php');
+        }
+
+        $fragment = new rex_fragment([
+            'label' => rex_i18n::msg('quick_navigation_favorite'),
+            'icon' => 'fa-regular fa-star',
+            'listItems' => $listItems,
+        ]);
+        return $fragment->parse('QuickNavigation/Dropdown.php');
     }
 }

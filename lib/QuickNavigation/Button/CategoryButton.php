e788a05de761da4564a2d0ea9c9cd253c1e5bd41
Thomas Skerbis <thomas.skerbis@mac.com>
2024-06-04 19:26:35 +0200

    removed NavigationBuidler

--- lib/QuickNavigation/Button/CategoryButton.php
+++ lib/QuickNavigation/Button/CategoryButton.php
@@ -13,94 +13,10 @@ use rex_i18n;
 use rex_string;
 use rex_url;
 use rex_yrewrite;
+use FriendsOfRedaxo\QuickNavigation\Utility\BuildNavigationArray;
 
 class CategoryButton implements ButtonInterface
 {
-    /**
-     * @return array<mixed>
-     */
-    public function generateBackendNavArray(int $clangId = null, bool $ignoreOffline = true, int $parentId = null, bool $includeHome = true): array
-    {
-        $user = rex::getUser();
-        if ($clangId === null) {
-            $clangId = rex_clang::getCurrentId();
-        }
-
-        $backendContext = rex_context::fromGet();
-        $backendContext->setParam('rex-api-call', 0);
-        $backendContext->setParam('clang', $clangId);
-        if ($backendContext->getParam('page') !== 'content/edit') {
-            if ($backendContext->getParam('page') !== 'linkmap') {
-                $backendContext->setParam('page', 'structure');
-            }
-        }
-        $articleId = rex_request('article_id', 'int');
-        $currentId = rex_request('category_id', 'int', $articleId);
-        if ($article = rex_article::get($articleId)) {
-            $currentId = $article->getCategoryId();
-        }
-
-        $categoriesArray = [];
-
-        if ($includeHome) {
-            $categoriesArray[] = [
-                'id' => 0,
-                'name' => rex_i18n::msg('root_level'),
-                'current' => $currentId === 0,
-                'domain' => 'default',
-                'url' => rex_url::backendPage('structure', ['clang' => $clangId]),
-                'children' => [],
-            ];
-        }
-
-        $categories = [];
-
-        if ($parentId === null) {
-            $mountpoints = $user->getComplexPerm('structure')->getMountpoints();
-            if (!empty($mountpoints)) {
-                foreach ($mountpoints as $mpId) {
-                    if ($mpCategory = rex_category::get($mpId, $clangId)) {
-                        $categories[] = $mpCategory;
-                    }
-                }
-            } else {
-                $categories = rex_category::getRootCategories($ignoreOffline, $clangId);
-            }
-        } elseif ($parentCategory = rex_category::get($parentId, $clangId)) {
-            $categories = $parentCategory->getChildren($ignoreOffline);
-        }
-
-        foreach ($categories as $category) {
-            if (!$user->getComplexPerm('structure')->hasCategoryPerm($category->getId())) {
-                continue;
-            }
-
-            $categoryId = $category->getId();
-            $backendContext->setParam('category_id', $categoryId);
-            $backendContext->setParam('article_id', $categoryId);
-            $domainName = '';
-            if (rex_addon::get('yrewrite')->isAvailable()) {
-                $domainName = rex_escape(rex_yrewrite::getDomainByArticleId($categoryId)->getName());
-            }
-
-            $current = $categoryId == $currentId;
-
-            $categoriesArray[] = [
-                'id' => $category->getId(),
-                'name' => $category->getName(),
-                'current' => $current,
-                'domain' => $domainName,
-                'url' => $backendContext->getUrl(),
-                'children' => self::generateBackendNavArray($clangId, $ignoreOffline, $category->getId(), false),
-            ];
-        }
-
-        return $categoriesArray;
-    }
-
-    /**
-     * @param  array<mixed> $categoriesArray
-     */
     public function renderCategoriesAsList(array $categoriesArray, int $depth = 0): array
     {
         $listItems = [];
@@ -122,9 +38,8 @@ class CategoryButton implements ButtonInterface
                 </a>';
 
             if (!empty($item['children'])) {
-                // Erhöhe die Tiefe um 1 für die Kinder
                 $fragment = new rex_fragment([
-                    'listItems' => self::renderCategoriesAsList($item['children'], $depth + 1),
+                    'listItems' => $this->renderCategoriesAsList($item['children'], $depth + 1),
                 ]);
                 $listItem .= $fragment->parse('QuickNavigation/List.php');
             }
@@ -144,9 +59,9 @@ class CategoryButton implements ButtonInterface
         }
 
         $currentClangId = rex_clang::getCurrentId();
-        $categoriesArray = self::generateBackendNavArray($currentClangId, $ignoreOffline, null); // Argumente nach Bedarf anpassen
+        $categoriesArray = BuildNavigationArray::generateBackendNavArray($currentClangId, $ignoreOffline, null);
 
-        $listItems = self::renderCategoriesAsList($categoriesArray);
+        $listItems = $this->renderCategoriesAsList($categoriesArray);
         $placeholder = rex_i18n::msg('quick_navigation_placeholder');
         $fragment = new rex_fragment();
         $fragment->setVar('id', 'quick-navigation-search');

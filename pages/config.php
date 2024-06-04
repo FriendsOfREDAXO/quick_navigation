1040281ef29f1ee38cf2263c337a04ec101f41f0
Thomas Skerbis <thomas.skerbis@mac.com>
2024-06-04 19:05:18 +0200

    renaming from quicknavi to quick-navigation or quick_navigation

--- pages/config.php
+++ pages/config.php
@@ -20,14 +20,14 @@ $user = rex::getUser()->getId();
 // Einstellungen speichern
 if (rex_post('formsubmit', 'string') == '1') {
     $package->setConfig(rex_post('config', [
-        ['quicknavi_favs' . $user, 'array[int]'],
-        ['quicknavi_ignoreoffline' . $user, 'int'],
-        ['quicknavi_artdirections' . $user, 'int'],
+        ['quick_navigation_favs' . $user, 'array[int]'],
+        ['quick_navigation_ignoreoffline' . $user, 'int'],
+        ['quick_navigation_artdirections' . $user, 'int'],
     ]));
-    echo rex_view::success($package->i18n('quicknavi_config_saved'));
+    echo rex_view::success($package->i18n('quick_navigation_config_saved'));
 }
 
-$content .= '<fieldset><legend>' . $package->i18n('quicknavi_info') . '</legend>';
+$content .= '<fieldset><legend>' . $package->i18n('quick_navigation_info') . '</legend>';
 // Kategorieauswahl
 $formElements[] = $n;
 $fragment = new rex_fragment();
@@ -36,17 +36,17 @@ $content .= $fragment->parse('core/form/container.php');
 // Kategorienauswahl
 $formElements = [];
 $n = [];
-$n['label'] = '<label for="quicknavi-config-favs">' . $package->i18n('quicknavi_categories') . '</label>';
+$n['label'] = '<label for="quick-navinigation-config-favs">' . $package->i18n('quick_navigation_categories') . '</label>';
 $category_select = new rex_category_select(false, false, true, true);
-$category_select->setName('config[quicknavi_favs' . $user . '][]');
-$category_select->setId('quicknavi-config-favs');
+$category_select->setName('config[quick_navigation_favs' . $user . '][]');
+$category_select->setId('quick-navinigation-config-favs');
 $category_select->setSize('10');
 $category_select->setMultiple(true);
 $category_select->setAttribute('class', 'selectpicker show-menu-arrow form-control');
 $category_select->setAttribute('data-actions-box', 'false');
 $category_select->setAttribute('data-live-search', 'true');
 $category_select->setAttribute('data-size', '15');
-$category_select->setSelected($package->getConfig('quicknavi_favs' . $user));
+$category_select->setSelected($package->getConfig('quick_navigation_favs' . $user));
 $n['field'] = $category_select->get();
 $formElements[] = $n;
 $fragment = new rex_fragment();
@@ -55,8 +55,8 @@ $content .= $fragment->parse('core/form/container.php');
 // Ignore offline cats
 $formElements = [];
 $n = [];
-$n['label'] = '<label for="quicknavi-forcal-checkbox">' . $package->i18n('quicknavi_ignoreoffline') . '</label>';
-$n['field'] = '<input type="checkbox" id="quicknavi-forcal-checkbox" name="config[quicknavi_ignoreoffline' . $user . ']"' . (!empty($package->getConfig('quicknavi_ignoreoffline' . $user)) && $package->getConfig('quicknavi_ignoreoffline' . $user) == '1' ? ' checked="checked"' : '') . ' value="1" />';
+$n['label'] = '<label for="quick-navinigation-forcal-checkbox">' . $package->i18n('quick_navigation_ignoreoffline') . '</label>';
+$n['field'] = '<input type="checkbox" id="quick-navinigation-forcal-checkbox" name="config[quick_navigation_ignoreoffline' . $user . ']"' . (!empty($package->getConfig('quick_navigation_ignoreoffline' . $user)) && $package->getConfig('quick_navigation_ignoreoffline' . $user) == '1' ? ' checked="checked"' : '') . ' value="1" />';
 $formElements[] = $n;
 $fragment = new rex_fragment();
 $fragment->setVar('elements', $formElements, false);
@@ -65,8 +65,8 @@ $content .= $fragment->parse('core/form/checkbox.php');
 // Enable / Disable article directions
 $formElements = [];
 $n = [];
-$n['label'] = '<label for="quicknavi-article-checkbox">' . $package->i18n('quicknavi_artdirections') . '</label>';
-$n['field'] = '<input type="checkbox" id="quicknavi-article-checkbox" name="config[quicknavi_artdirections' . $user . ']"' . (!empty($package->getConfig('quicknavi_artdirections' . $user)) && $package->getConfig('quicknavi_artdirections' . $user) == '1' ? ' checked="checked"' : '') . ' value="1" />';
+$n['label'] = '<label for="quick-navinigation-article-checkbox">' . $package->i18n('quick_navigation_artdirections') . '</label>';
+$n['field'] = '<input type="checkbox" id="quick-navinigation-article-checkbox" name="config[quick_navigation_artdirections' . $user . ']"' . (!empty($package->getConfig('quick_navigation_artdirections' . $user)) && $package->getConfig('quick_navigation_artdirections' . $user) == '1' ? ' checked="checked"' : '') . ' value="1" />';
 $formElements[] = $n;
 $fragment = new rex_fragment();
 $fragment->setVar('elements', $formElements, false);
@@ -75,7 +75,7 @@ $content .= $fragment->parse('core/form/checkbox.php');
 // Save-Button
 $formElements = [];
 $n = [];
-$n['field'] = '<button class="btn btn-save rex-form-aligned" type="submit" name="save" value="' . $package->i18n('quicknavi_config_save') . '">' . $package->i18n('quicknavi_config_save') . '</button>';
+$n['field'] = '<button class="btn btn-save rex-form-aligned" type="submit" name="save" value="' . $package->i18n('quick_navigation_config_save') . '">' . $package->i18n('quick_navigation_config_save') . '</button>';
 $formElements[] = $n;
 $fragment = new rex_fragment();
 $fragment->setVar('elements', $formElements, false);
@@ -88,7 +88,7 @@ $buttons = '
 // Ausgabe Formular
 $fragment = new rex_fragment();
 $fragment->setVar('class', 'edit');
-$fragment->setVar('title', $package->i18n('quicknavi_general'));
+$fragment->setVar('title', $package->i18n('quick_navigation_general'));
 $fragment->setVar('body', $content, false);
 $fragment->setVar('buttons', $buttons, false);
 $output = $fragment->parse('core/page/section.php');

a2ec54dd082fa388f415ecc9eeec706b9f767ff5
Thomas Skerbis <thomas.skerbis@mac.com>
2024-06-04 22:59:44 +0200

    dead code deleted and some renames

--- lib/QuickNavigation/LinkMap/QuickNavigationLinkMap.php
+++ lib/QuickNavigation/LinkMap/QuickNavigationLinkMap.php
@@ -16,25 +16,17 @@ class QuickNavigationLinkMap
      */
     public static function LinkMapNavigation(rex_extension_point $ep): ?string
     {
-        // get article history
         if (rex_be_controller::getCurrentPagePart(1) == 'linkmap') {
-            $custom = '';
-            $drophistory = '';
-            // Check if language is set
-            $qlang = rex_request('clang', 'int', 0);
-            if ($qlang == 0 || $qlang == '') {
-                $qlang = 1;
-            }
 
-            $catsbutton = new CategoryButton();
-            $catsbutton_output = $catsbutton->get();
+            $category_list = new CategoryButton();
+            $category_button_output = $category_listn->get();
 
-            $history = new ArticleHistoryButton('linkmap', 15);
-            $drophistory = $history->get();
+            $history_list = new ArticleHistoryButton('linkmap', 15);
+            $history_list_output = $history_list->get();
+            
             $custom_linkmap_buttons = rex_extension::registerPoint(new rex_extension_point('QUICK_LINKMAP_CUSTOM', $custom));
-            return '<div class="btn-group quick-navigation-btn-group linkmapbt pull-right">' . $drophistory . $catsbutton_output . $custom_linkmap_buttons . '</div>' . $ep->getSubject();
+            return '<div class="btn-group quick-navigation-btn-group linkmapbt pull-right">' . $history_list_output .   $category_button_output . $custom_linkmap_buttons . '</div>' . $ep->getSubject();
         }
-
         return null;
     }
 }

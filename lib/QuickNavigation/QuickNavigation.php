48829934d2908d031855812358be372264b2150f
Thomas Skerbis <thomas.skerbis@mac.com>
2024-06-04 22:39:34 +0200

    Update QuickNavigation.php

--- lib/QuickNavigation/QuickNavigation.php
+++ lib/QuickNavigation/QuickNavigation.php
@@ -2,46 +2,27 @@
 
 namespace FriendsOfRedaxo\QuickNavigation;
 
-use FriendsOfRedaxo\QuickNavigation\Button\ArticleHistoryButton;
 use FriendsOfRedaxo\QuickNavigation\Button\ButtonRegistry;
-use FriendsOfRedaxo\QuickNavigation\Button\CategoryButton;
-use rex;
-use rex_be_controller;
-use rex_extension;
+use FriendsOfRedaxo\QuickNavigation\LinkMap\QuickNavigationLinkMap;
+use FriendsOfRedaxo\QuickNavigation\Media\QuickNavigationMedia;
 use rex_extension_point;
-use rex_formatter;
-use rex_fragment;
-use rex_sql;
-use rex_string;
-use rex_url;
 
 class QuickNavigation
 {
     /**
-     * @param rex_extension_point<string> $ep
-     */
-    public static function LinkMapQuickNavigation(rex_extension_point $ep): ?string
+    * @deprecated since version 7.0, will be removed in version 9.0. Use <FriendsOfRedaxo\QuickNavigation\ApiFunction\MenuRender> instead.
+    */
+    public static function linkmap_list(rex_extension_point $ep): ?string
     {
-        // get article history
-        if (rex_be_controller::getCurrentPagePart(1) == 'linkmap') {
-            $custom = '';
-            $drophistory = '';
-            // Check if language is set
-            $qlang = rex_request('clang', 'int', 0);
-            if ($qlang == 0 || $qlang == '') {
-                $qlang = 1;
-            }
-
-            $catsbutton = new CategoryButton();
-            $catsbutton_output = $catsbutton->get();
-
-            $history = new ArticleHistoryButton('linkmap', 15);
-            $drophistory = $history->get();
-            $custom_linkmap_buttons = rex_extension::registerPoint(new rex_extension_point('QUICK_LINKMAP_CUSTOM', $custom));
-            return '<div class="btn-group quick-navigation-btn-group linkmapbt pull-right">' . $drophistory . $catsbutton_output . $custom_linkmap_buttons . '</div>' . $ep->getSubject();
-        }
-
-        return null;
+        return QuickNavigationLinkMap::LinkMapQuickNavigation($ep);
+    }
+  
+    /**
+    * @deprecated since version 7.0, will be removed in version 9.0. Use <FriendsOfRedaxo\QuickNavigation\ApiFunction\MenuRender> instead.
+    */
+    public static function media_history(rex_extension_point $ep): ?string
+    {
+        return QuickNavigationMedia::MediaHistory($ep);
     }
 
     public static function get(): string

542afafaca14dcf765077cbe37f5cb80b3d1b56b
Thomas Skerbis <thomas.skerbis@mac.com>
2024-06-04 21:32:49 +0200

    Update rex_api_quicknavigation_render.php

--- lib/QuickNavigation/rex_api_quicknavigation_render.php
+++ lib/QuickNavigation/rex_api_quicknavigation_render.php
@@ -3,7 +3,7 @@
 use FriendsOfRedaxo\QuickNavigation\ApiFunction\MenuRender;
 
 /**
- * @deprecated since version 7.0, will be removed in version 9.0. Use <FriendsOfRedaxo\QuickNavigation\ApiFunction\MenuRender> instead.
+ * @deprecated since version 7.0, will be removed in a future release. Use <FriendsOfRedaxo\QuickNavigation\ApiFunction\MenuRender> instead.
  */
 class rex_api_quicknavigation_render extends MenuRender {
 }

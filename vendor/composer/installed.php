598ed6278be440fcf580c4b79d6aa92f864531c5
skerbis <skerbis@users.noreply.github.com>
2025-05-02 10:04:08 +0000

    Apply php-cs-fixer changes

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '0eb027ec73cc3b46e798d5fbec43df941bf6a7e5',
+        'reference' => '92b318b57fe854cbb361fc4ea748b70524b6b445',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '0eb027ec73cc3b46e798d5fbec43df941bf6a7e5',
+            'reference' => '92b318b57fe854cbb361fc4ea748b70524b6b445',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),

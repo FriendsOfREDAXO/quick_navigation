bd814c6b2014852c0b985173385879f18c9cd6bd
christophboecker <christophboecker@users.noreply.github.com>
2025-10-02 12:46:46 +0000

    Apply php-cs-fixer changes

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '99e2fa9c086ec064cd49b5c025ccecc7a0fb51e3',
+        'reference' => 'fa065100a38013a2dce528049d557345e5df3447',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '99e2fa9c086ec064cd49b5c025ccecc7a0fb51e3',
+            'reference' => 'fa065100a38013a2dce528049d557345e5df3447',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
